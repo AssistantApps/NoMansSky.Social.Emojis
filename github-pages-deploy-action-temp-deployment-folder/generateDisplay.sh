1241df8543d782fd895051326c07cb8e70462455
Khaoz-Topsy <hi@kurtlourens.com>
2023-02-06 17:23:56 +0000

    Deploying to gh-pages from @ AssistantApps/NoMansSky.Social.Emojis@4d2023addfbf0536b2e6aa8466c7e5b4ec3b3990 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/generateDisplay.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+numColumns=3;
+search_dir=./emoji;
+
+resultStr="# No Man's Sky Social emojis\n\n";
+resultStr="${resultStr}Here is a list of the currently available emojis on our Mastodon instance\n\n";
+
+tableHead="";
+tableHeadDef="";
+columnCounter=0;
+for i in $(seq 1 $numColumns); 
+do 
+    ((columnCounter+=1));
+    tableHead="${tableHead}| Name | Image | ";
+    tableHeadDef="${tableHeadDef}| :---: | :---: ";
+    
+    if [[ columnCounter -lt numColumns ]]; then
+        tableHeadDef="${tableHeadDef}| :---: ";
+    fi
+done
+resultStr="${resultStr}${tableHead}\n";
+resultStr="${resultStr}${tableHeadDef}|\n";
+
+fileCounter=0;
+for entry in "$search_dir"/*
+do
+    if [[ "$entry" == *".png"* || "$entry" == *".jpg"* || "$entry" == *".gif"* || "$entry" == *".webp"* ]]; then
+        ((fileCounter+=1));
+        
+        prefix="/emoji/";
+        fileName=${entry//$prefix/""};
+        fileNameArr=(${fileName//./ })
+        tag=${fileNameArr[0]};
+        resultStr="${resultStr}| :${tag}: | <img alt=\"${tag}\" src=\"${entry}\" height=\"50px\" /> | ";
+        
+        if [[ fileCounter -eq numColumns ]]; then
+            resultStr="${resultStr}\n";
+            fileCounter=0;
+        fi
+    fi
+done
+
+echo -e "$resultStr" > README.md
